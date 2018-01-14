5da17a1da717eacb68d4d4acc1099bb2ba933f4a
jbull328 <jackbull328@gmail.com>
2018-01-13 22:35:19 -0800

    initial commit before hosting

---
+++ src/MoviesList.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+
+import Movie from './Movie';
+
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=cd3d87baccc2047bf23e35c24b99cafa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({ // eslint-disable-line react/no-did-mount-set-state
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
